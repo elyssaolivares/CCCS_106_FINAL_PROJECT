ce71021da2c4e2b76696851447b5678301385b88
Elyssa Olivares <elolivares@my.cspc.edu.ph>
2026-03-15 16:10:55 +0800

    Update CLIENT_SECRETS_FILE path to relative

--- app/services/google/google_auth.py
+++ app/services/google/google_auth.py
@@ -4,7 +4,7 @@ from google_auth_oauthlib.flow import Flow
 from urllib.parse import urlparse, parse_qs
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
-CLIENT_SECRETS_FILE = r"/home/ayskopi/Documents/CCCS_106_FINAL_PROJECT/app/services/google/client_secret.json"
+CLIENT_SECRETS_FILE = os.path.join(os.path.dirname(__file__), "client_secret.json")
 SCOPES = [
     "openid",
     "https://www.googleapis.com/auth/userinfo.email",
